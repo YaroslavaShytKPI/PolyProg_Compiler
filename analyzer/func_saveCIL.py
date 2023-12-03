a8306d8ae25758d865151464355df69f5b2668cf
Daria Sydorenko <dashasidorenko123@gmail.com>
2023-12-03 16:44:07 +0200

    Треба доробити .il

--- analyzer/func_saveCIL.py
+++ analyzer/func_saveCIL.py
@@ -36,17 +36,77 @@ def saveCIL(fileName):
   for x in table_of_vars:
     index,tp,_ = table_of_vars[x][0], table_of_vars[x][1], table_of_vars[x][2]
     if tp == 'int': tpil = 'int32' 
-    elif tp == 'double': tpil = 'double32'
-    else: tpil = 'bool32'
+    elif tp == 'double': tpil = 'float32'
+    else: tpil = 'bool'
     if index == cntVars: comma = "\n     )"
-    localVars += "       [{0}]  {1} {2}".format(index-0,tpil, x) + comma + "\n"
+    localVars += "       [{0}]  {1} {2}".format(index-1,tpil, x) + comma + "\n"
   # print((x,a))
   entrypoint = """
    .entrypoint
    //.maxstack  8\n"""
   code = ""
-  for instr in postfix_code:
-    code +=  " ".join(map(str, instr)) + "\n"
+  current_token = None
+
+  for token, token_type in postfix_code:
+    if current_token:
+        if token_type == 'jump':
+            code += f"   br    {current_token}\n"
+        elif token_type == 'jf':
+            code += f"   brfalse    {current_token}\n"
+        else:
+            code += f"{current_token}:\n"
+        current_token = None
+        continue
+    
+    if token_type == 'label':
+        current_token = token
+        continue
+    
+    if token_type == 'l-val':
+        code += f"   ldloca    {token}\n"
+    elif token_type == 'r-val' or token_type == 'id':
+        code += f"   ldloc    {token}\n"
+    elif token_type == 'int':
+        code += f"   ldc.i4    {token}\n"
+    elif token_type == 'double':
+        code += f"   ldc.r4    {token}\n"
+    elif token_type == 'assign_op':
+        code += f"   stind.{('i4' if token == 'int' else 'r4')}\n"
+    elif token_type == 'print':
+        code += f"   call    void [mscorlib]System.Console::WriteLine()\n"
+    elif token_type == 'readline':
+        code += f"   call    string [mscorlib]System.Console::ReadLine()\n"
+    elif token_type == 'rel_op':
+        if token == '>':
+            code += "   cgt\n"
+        elif token == '<':
+            code += "   clt\n"
+        elif token == '>=':
+            code += "    clt\n    ldc.i4.0\n    ceq\n"
+        elif token == '<=':
+            code += "    cgt\n    ldc.i4.0\n    ceq\n"
+        elif token == '==':
+            code += "   ceq\n"
+        elif token == '!=':
+            code += "    ceq\n    ldc.i4.0\n    ceq\n"
+    elif token_type == 'mult_op' or token_type == 'add_op':
+        if token == '+':
+            code += "   add\n"
+        elif token == '-':
+            code += "   sub\n"
+        elif token == '*':
+            code += "   mul\n"
+        elif token == '/':
+            code += "   div\n"
+    elif token_type == 'power_op':
+        # code += "    conv.r8\n      conv.r8\n      pow\n      conv.r4\n"
+        code += "   pow\n"
+    elif token_type == 'bool':
+        code += f"   ldstr \"{token}\"\n"
+    elif token_type == 'negate_op':
+        code += "   neg\n"
+    else:
+        code += f"{token}!!!"
     
   # виведення значень змінних
   values = ""
